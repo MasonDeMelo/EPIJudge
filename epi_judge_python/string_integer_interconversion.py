b6ccedbda353e6d48280d6c39587758345321e1a
Mason DeMelo <demelomason@gmail.com>
2020-09-24 00:39:51 +0000

    6.01

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,13 +3,54 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    if x == 0:
+        return "0"
+    result = "" 
+    negative = x < 0
+    x = abs(x)
+    while x:
+        result = digit_to_char(x%10) + result
+        x = x//10 
+    return ("-" if negative else "") + result
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    sf = 1
+    result = 0
+    for char in reversed(s):
+        if char == "-":
+            return -result
+        elif char == "+":
+            print(result)
+        else:
+            result += sf * char_to_digit(char)
+            sf *= 10
+    return result 
+
+def char_to_digit(c: str) -> int:
+    return {"0" : 0,
+            "1" : 1,
+            "2" : 2,
+            "3" : 3,
+            "4" : 4,
+            "5" : 5,
+            "6" : 6,
+            "7" : 7,
+            "8" : 8,
+            "9" : 9}[c]
+
+def digit_to_char(d: int) -> str:
+    return {0 : "0",
+            1 : "1",
+            2 : "2",
+            3 : "3",
+            4 : "4",
+            5 : "5",
+            6 : "6",
+            7 : "7",
+            8 : "8",
+            9 : "9"}[d]
+
 
 
 def wrapper(x, s):
--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -490,7 +490,7 @@ problem_mapping = {
                 "total": 15002
             },
             "Python: string_integer_interconversion.py": {
-                "passed": 0,
+                "passed": 15002,
                 "total": 15002
             }
         },
